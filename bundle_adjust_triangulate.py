6aeb99e6aeee4445ce67f6f90e20c39adc6e6477
Lili Karashchuk <krchtchk@gmail.com>
2026-04-27 19:37:20 -0400

    fix the bundle adjustment to fit in L4

--- bundle_adjust_triangulate.py
+++ bundle_adjust_triangulate.py
@@ -71,6 +71,7 @@ def main():
         frames_dict[cname] = pd.merge_asof(
             stamps, d, on="timestamp", direction="nearest", tolerance=pd.Timedelta(seconds=1 / 15.0)
         )
+        
 
     all_p2ds = []
     scores = []
@@ -87,12 +88,14 @@ def main():
 
     cgroup = CameraGroup.load(calib_fname_init).to("cuda:0")
 
-    p2d_cuda = torch.as_tensor(all_p2ds, device="cuda:0")
-    scores_cuda = torch.as_tensor(scores, device="cuda:0")
-    p2d_cuda[scores_cuda < 0.95] = torch.nan
+    # p2d_cuda = torch.as_tensor(all_p2ds, device="cuda:0")
+    # scores_cuda = torch.as_tensor(scores, device="cuda:0")
+    # p2d_cuda[scores_cuda < 0.95] = torch.nan
 
-    count = torch.sum(torch.isfinite(p2d_cuda[:, :, 0]), axis=0)
-    p2d_sub = p2d_cuda[:, count >= 2]
+    count = np.sum(np.isfinite(all_p2ds[:, :, 0]), axis=0)
+    p2d_sub = all_p2ds[:, count >= 2].copy()
+    scores_sub = scores[:, count >= 2]
+    p2d_sub[scores_sub < 0.90] = np.nan
 
     print(p2d_sub.shape)
 
@@ -100,25 +103,31 @@ def main():
     print(err.item())
 
     cgroup.bundle_adjust_iter(p2d_sub, verbose=True, only_extrinsics=True,
-                              n_samp_iter=500, n_iters=10, n_samp_full=1000)
+                              n_samp_iter=400, n_iters=16, n_samp_full=1000)
 
     cgroup.dump(calib_fname_out)
 
-    p2d_tri = torch.as_tensor(all_p2ds, device="cuda:0")
-    p2d_tri[scores_cuda < 0.90] = torch.nan
+    # p2d_tri = all_p2ds.copy()
+    # p2d_tri[scores < 0.90] = torch.nan
 
     with torch.no_grad():
-        p3d = cgroup.triangulate(p2d_tri, progress=True)
+        p3d = cgroup.triangulate(p2d_sub, progress=True)
 
     p3d_numpy = p3d.detach().cpu().numpy()
-    err = cgroup.reprojection_error(p3d, p2d_cuda, mean=True).detach().cpu().numpy()
+    err = cgroup.reprojection_error(p3d, p2d_sub, mean=True).detach().cpu().numpy()
 
+    p3d_out = np.full((all_p2ds.shape[1], 3), np.nan)
+    p3d_out[count >= 2] = p3d_numpy
+
+    err_out = np.full((all_p2ds.shape[1],), np.nan)
+    err_out[count >= 2] = err
+    
     start_time = str(stamps["timestamp"][0])
     time_offset = (stamps["timestamp"] - stamps["timestamp"][0]) / pd.Timedelta(seconds=1.0)
 
-    np.savez_compressed(points_fname_out, p3d=p3d_numpy, err=err,
+    np.savez_compressed(points_fname_out, p3d=p3d_out, err=err_out,
                         p2d=all_p2ds, scores=scores, start_time=start_time,
-                        time_offset=time_offset, count=count.detach().cpu().numpy())
+                        time_offset=time_offset, count=count)
 
 
 if __name__ == "__main__":
